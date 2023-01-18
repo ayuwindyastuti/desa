0a137caa1d3bf3859391ba1b65308c09df6a9104
ayuwindyastuti <ayuwindyastuti@gmail.com>
2023-01-18 09:51:21 +0700

    update 18 jan 23

--- js/custom.js
+++ js/custom.js
@@ -23,58 +23,7 @@ $(document).ready(function() {
 		} else {
 			$(".navbar").removeClass("scroll");
 		}
-	});
-	
-	/* Dropdown navbar 
-	$(".dropdown-toggle").dropdown(); */
-
-	/* slick slide news */
-	$(".slick-news").slick({
-		dots: true,
-		autoplay: false,
-		slidesToShow: 4,
-		slidesToScroll: 4,
-		customPaging : function(slider, i){
-			var thumb = $(slider.$slides[i]).data();
-			return '<a class="dot">'+(i+1)+'</a>';
-		},
-		responsive: [
-		  {
-			breakpoint: 1200,
-			settings: {	
-			  infinite: false,
-			  slidesToShow: 3,
-			  slidesToScroll: 3,
-			  infinite: true,
-			  dots: true
-			}
-		  },
-		  {
-			breakpoint: 992,
-			settings: {
-			  slidesToShow: 2,
-			  slidesToScroll: 2
-			}
-		  },
-		  {
-			breakpoint: 576,
-			settings: {
-			  slidesToShow: 1,
-			  slidesToScroll: 1
-			}
-		  }
-		]
-	});
-
-	/* slick slide potensi desa */
-	$(".slick-pd").slick({
-		dots: true,
-		autoplay: false,
-		arrows: false,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-	});
-	  
+	}); 
 });
 function switchStyle() {
 	if (document.getElementById("styleSwitch").checked) {
